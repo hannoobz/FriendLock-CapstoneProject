b9013965bd8ff579fa7aa0155813c34bf311ca41
hannoobz <hanieffatkhan@gmail.com>
2025-04-28 15:00:12 +0700

    fix: ignore system UI block & add delay

--- app/src/main/java/com/hannoobz/friendlock/utils/AppDetectionService.kt
+++ app/src/main/java/com/hannoobz/friendlock/utils/AppDetectionService.kt
@@ -4,46 +4,50 @@ import android.accessibilityservice.AccessibilityService
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
-import android.os.Build
+import android.provider.Settings
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import android.view.inputmethod.InputMethodManager
 import com.hannoobz.friendlock.BlockActivity
 import com.hannoobz.friendlock.data.DBProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class AppDetectionService : AccessibilityService() {
     private var previousAppPackage: String? = null
     private val APP_BLOCKER_PACKAGE_NAME = "com.hannoobz.friendlock"
+    private val SYSTEM_UI_NAME = "com.android.systemui"
 
     override fun onServiceConnected() {
         super.onServiceConnected()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                "accessibility_service_channel",
-                "Accessibility Service",
-                NotificationManager.IMPORTANCE_LOW
-            )
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager.createNotificationChannel(channel)
-        }
+        val channel = NotificationChannel(
+            "accessibility_service_channel",
+            "Accessibility Service",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
 
         val notification = Notification.Builder(this, "accessibility_service_channel")
             .setContentTitle("")
             .setContentText("")
             .setOngoing(true)
             .build()
+
         startForeground(1, notification)
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        val currentInputMethod = Settings.Secure.getString(contentResolver, Settings.Secure.DEFAULT_INPUT_METHOD)
         if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
             val currentAppPackage = event.packageName?.toString()
             if (currentAppPackage != null) {
                 if (currentAppPackage != previousAppPackage) {
-                    if (currentAppPackage!=APP_BLOCKER_PACKAGE_NAME) {
+                    if ((currentAppPackage != APP_BLOCKER_PACKAGE_NAME) && (currentAppPackage!=SYSTEM_UI_NAME) && (!currentInputMethod.contains(currentAppPackage, ignoreCase = true))) {
                         previousAppPackage = currentAppPackage
                         checkAndBlockApp(currentAppPackage)
                     }
@@ -60,9 +64,11 @@ class AppDetectionService : AccessibilityService() {
             val appName = DBProvider.getDatabase(applicationContext)
                 .appDao()
                 .getAppByPackageName(packageName)
-
             if (appName != null && appName.isChecked) {
-                showOverlay()
+                CoroutineScope(Dispatchers.Main).launch {
+                    delay(1500)
+                    showOverlay()
+                }
             } else {
             }
         }
