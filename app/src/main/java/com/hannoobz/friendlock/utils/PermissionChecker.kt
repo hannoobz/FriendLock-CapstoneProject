280a25b31dbc9b8ad30053cf74311a1c3e954c4f
hannoobz <hanieffatkhan@gmail.com>
2025-05-01 10:52:24 +0700

    refactor: separate permission checker

--- app/src/main/java/com/hannoobz/friendlock/utils/PermissionChecker.kt
+++ app/src/main/java/com/hannoobz/friendlock/utils/PermissionChecker.kt
@@ -1,2 +1,56 @@
 package com.hannoobz.friendlock.utils
 
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import android.view.accessibility.AccessibilityManager
+import java.util.Locale
+
+fun hasUsageStatsPermission(context: Context): Boolean {
+    val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as android.app.AppOpsManager
+    val mode = appOps.checkOpNoThrow(
+        android.app.AppOpsManager.OPSTR_GET_USAGE_STATS,
+        android.os.Process.myUid(),
+        context.packageName
+    )
+    return mode == android.app.AppOpsManager.MODE_ALLOWED
+}
+
+fun isAccessibilityServiceEnabled(context: Context): Boolean {
+    val accessibilityManager =
+        context.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+    val componentName = ComponentName(context, AppDetectionService::class.java)
+    return accessibilityManager
+        .getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
+        .any {
+            it.resolveInfo.serviceInfo.name == componentName.className
+        }
+}
+
+fun openUsageAccessSettings(context: Context) {
+    val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    context.startActivity(intent)
+}
+
+fun openAccessibilitySettings(context: Context) {
+    val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    context.startActivity(intent)
+}
+
+fun openOverlayPermissionSettings(context: Context) {
+    if ("xiaomi" == Build.MANUFACTURER.lowercase(Locale.ROOT)) {
+        val intent = Intent("miui.intent.action.APP_PERM_EDITOR")
+        intent.setClassName("com.miui.securitycenter",
+            "com.miui.permcenter.permissions.PermissionsEditorActivity")
+        intent.putExtra("extra_pkgname", context.packageName)
+        context.startActivity(intent)
+    } else {
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+        context.startActivity(intent)
+    }
+}
